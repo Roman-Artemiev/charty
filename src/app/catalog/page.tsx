57e8f4525be8e0f84737b2cce4302d7e1ef45bf0
Roman Artemiev <115183967+Roman-Artemiev@users.noreply.github.com>
2024-07-27 14:07:40 +0200

    Remove catalog filters

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,29 +1,14 @@
 "use client";
 
-import Header from '@/components/header/Header'
-import { Box, Heading, Text, 
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider, 
-  Flex,
-  Button,
-  Center,
-  Grid,
-} from '@chakra-ui/react'
-import { IoIosArrowDown } from "react-icons/io";
-import { CiGrid41, CiGrid2H } from "react-icons/ci";
-import React, { lazy, useEffect, useRef, useState } from 'react'
-import { COLORS, TRANSITIONS } from './../../theme/index';
-import { GameCardHome } from '@/interface';
-import { loadGames } from '@/utils/loadGames';
-import { useInView } from 'react-intersection-observer';
+import Header from "@/components/header/Header";
+import { Box, Heading, Text, Flex, Grid } from "@chakra-ui/react";
+import React, { lazy, useEffect, useRef, useState } from "react";
+import { GameCardHome } from "@/interface";
+import { loadGames } from "@/utils/loadGames";
 
-const GameCatalogCard = lazy(() => import('@/components/cards/GameCatalogCard'));
+const GameCatalogCard = lazy(
+  () => import("@/components/cards/GameCatalogCard")
+);
 let pageNum = 2;
 
 const Catalog = () => {
@@ -34,11 +19,14 @@ const Catalog = () => {
 
   const fetchGames = async (page: number) => {
     setLoading(true);
-    const games = await loadGames({ page_size: 12, page: page }) as GameCardHome[];
-    setData(prevData => [...prevData, ...games]);
-    console.log("🚀 ~ games:", games);
+    const games = (await loadGames({
+      page_size: 12,
+      page: page,
+    })) as GameCardHome[];
+    setData((prevData) => [...prevData, ...games]);
+    // console.log("🚀 ~ games:", games);
     setLoading(false);
-  }
+  };
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -50,210 +38,109 @@ const Catalog = () => {
       },
       {
         root: null,
-        rootMargin: '0px',
+        rootMargin: "0px",
         threshold: 1.0,
       }
     );
-  
+
     if (loadMoreRef.current) {
       observer.observe(loadMoreRef.current);
     }
-  
+
     return () => {
       if (loadMoreRef.current) {
         observer.unobserve(loadMoreRef.current);
       }
     };
   }, [loading]);
-  
-  const sortByOptions = [
-    { filter: 'Relevance',}, 
-    { filter: 'Date added', }, 
-    { filter: 'Name', }, 
-    { filter: 'Release date' }, 
-    { filter: 'Popularity' }, 
-    { filter: 'Average rating', }, 
-  ]
-  const platformOptions = [
-    { filter: 'PC',}, 
-    { filter: 'PlayStation', }, 
-    { filter: 'Xbox', }, 
-    { filter: 'iOS' }, 
-    { filter: 'Android' }, 
-    { filter: 'Apple Macintosh', }, 
-    { filter: 'Linux', }, 
-    { filter: 'Nintendo', }, 
-  ]
 
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [columnCount, setColumnCount] = useState<number>(4);
 
   const columnSetting = (windowWidthCurrent: number) => {
     const minCarWidth = 320;
-    const columnCardPerScreen = Math.floor(windowWidthCurrent / minCarWidth) || 1;
+    const columnCardPerScreen =
+      Math.floor(windowWidthCurrent / minCarWidth) || 1;
     // console.log(windowWidthCurrent, "DEFAULT: ", columnCardPerScreen, "FLOOR: ", );
     return columnCardPerScreen > 4 ? 4 : columnCardPerScreen;
   };
 
   useEffect(() => {
     const handleResize = () => setWindowWidth(window.innerWidth);
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     // columnSetting(windowWidth);
     setColumnCount(columnSetting(windowWidth));
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, [windowWidth]);
 
-
-
   return (
     <>
-      <Header/>
+      <Header />
 
-      <Box className='wrapper' mt="60px">
+      <Box className="wrapper" mt="60px">
         <Box mb="50px">
           <Heading as="h1" mb="5px" fontSize="40px" fontWeight="800">
             New and trending
           </Heading>
 
-          <Text mb="30px" fontSize="16px">Based on player counts and release date</Text>
-
-          <Flex columnGap="50px" justifyContent="space-between">
-            <Flex columnGap="20px">
-              <Menu closeOnSelect={false}>
-                <MenuButton
-                  rightIcon={ <IoIosArrowDown width="20px" height="20px" /> }
-                  as={Button}
-                  w="200px" h="44px"
-                  color={COLORS.white}
-                  _active={{ bg: COLORS.dark }}
-                  bg={COLORS.darkLight}
-                  fontSize="16px"
-                  fontWeight="400"
-
-                  _hover={{ bg: COLORS.dark }}
-                >
-                  Relevance
-                </MenuButton>
-
-                <MenuList minWidth="240px" bgColor={COLORS.darkLight} defaultValue='pc' border={0} zIndex={3}>
-                  <MenuOptionGroup
-                    title="Order by:"
-                    type="checkbox"
-                    bgColor={COLORS.darkLight}
-                    color={COLORS.gray}
-                    fontWeight={400}
-                    transition={TRANSITIONS.mainTransition}
-                  >
-                    {sortByOptions.map((option) => (
-                      <MenuItem
-                        key={option.filter}
-                        name={option.filter}
-                        bgColor={COLORS.darkLight}
-                        color={COLORS.white}
-                        _hover={{ bgColor: COLORS.whiteTransparentLight }}
-                        transition={TRANSITIONS.mainTransition}
-                        // onClick={() => handlePlatformOptionClick(option.id)}
-                      >
-                        {option.filter}
-                      </MenuItem>
-                    ))}
-                  </MenuOptionGroup>
-                </MenuList>
-              </Menu>
-              
-              <Menu closeOnSelect={false}>
-                <MenuButton
-                  rightIcon={ <IoIosArrowDown width="20px" height="20px" /> }
-                  as={Button}
-                  w="200px" h="44px"
-                  color={COLORS.white}
-                  _active={{ bg: COLORS.dark }}
-                  bg={COLORS.darkLight}
-                  fontSize="16px"
-                  fontWeight="400"
-                  _hover={{ bg: COLORS.dark }}
-                >
-                  Platforms
-                </MenuButton>
-
-                <MenuList minWidth="240px" bgColor={COLORS.darkLight} defaultValue='pc' border={0} zIndex={3}>
-                  <MenuOptionGroup
-                    title="Platforms:"
-                    type="checkbox"
-                    bgColor={COLORS.darkLight}
-                    color={COLORS.gray}
-                    fontWeight={400}
-                    transition={TRANSITIONS.mainTransition}
-                  >
-                    {platformOptions.map((option) => (
-                      <MenuItem
-                        key={option.filter}
-                        name={option.filter}
-                        bgColor={COLORS.darkLight}
-                        color={COLORS.white}
-                        _hover={{ bgColor: COLORS.whiteTransparentLight }}
-                        transition={TRANSITIONS.mainTransition}
-                        // onClick={() => handlePlatformOptionClick(option.id)}
-                      >
-                        {option.filter}
-                      </MenuItem>
-                    ))}
-                  </MenuOptionGroup>
-                </MenuList>
-              </Menu>
-            </Flex>
-
-            <Flex alignItems="center" columnGap="20px">
-              <Text color={COLORS.whiteTransparent}>Display options: </Text>
-
-              <Flex columnGap="10px">
-                <Center w="44px" h="44px" bg={COLORS.darkLight} borderRadius="5px" cursor="pointer" _hover={{opacity: "0.8", transform: "translateY(-5px)", transition: TRANSITIONS.mainTransition}} transition={TRANSITIONS.mainTransition}>
-                  <CiGrid41 size="24px" />
-                </Center>
-
-                <Center w="44px" h="44px" bg={COLORS.darkLight} borderRadius="5px" cursor="pointer" opacity="0.6" _hover={{opacity: "0.8", transform: "translateY(-5px)", transition: TRANSITIONS.mainTransition}} transition={TRANSITIONS.mainTransition}>
-                  <CiGrid2H size="24px" />
-                </Center>
-              </Flex>
-            </Flex>
-          </Flex>
+          <Text mb="30px" fontSize="16px">
+            Based on player counts and release date
+          </Text>
         </Box>
-        
-        <Grid gap='30px' gridTemplateColumns={`repeat(${columnCount}, 1fr)`} gridAutoFlow='row'>
+
+        <Grid
+          gap="30px"
+          gridTemplateColumns={`repeat(${columnCount}, 1fr)`}
+          gridAutoFlow="row"
+        >
           {Array.from({ length: columnCount }).map((_, columnIndex) => {
             const itemsPerColumn = Math.floor(data?.length / columnCount);
 
             const startIndex = columnIndex * itemsPerColumn;
-            const endIndex = Math.min(startIndex + itemsPerColumn, data?.length);
+            const endIndex = Math.min(
+              startIndex + itemsPerColumn,
+              data?.length
+            );
             const columnData = data?.slice(startIndex, endIndex);
 
             return (
-              <Flex key={columnIndex} gap="30px" flexDirection='column'>
-                {columnData?.map(({ id, name, background_image, platforms, price, genres, released, rating }) => (
-                  <GameCatalogCard
-                    key={id}
-                    id={id}
-                    name={name}
-                    src={background_image}
-                    price={price}
-                    platforms={platforms}
-                    genres={genres}
-                    released={released}
-                    rating={rating}
-                  />
-                ))}
+              <Flex key={columnIndex} gap="30px" flexDirection="column">
+                {columnData?.map(
+                  ({
+                    id,
+                    name,
+                    background_image,
+                    platforms,
+                    price,
+                    genres,
+                    released,
+                    rating,
+                  }) => (
+                    <GameCatalogCard
+                      key={id}
+                      id={id}
+                      name={name}
+                      src={background_image}
+                      price={price}
+                      platforms={platforms}
+                      genres={genres}
+                      released={released}
+                      rating={rating}
+                    />
+                  )
+                )}
               </Flex>
-            )
+            );
           })}
-          <div ref={loadMoreRef} style={{ height: '50px' }}></div>
+          <div ref={loadMoreRef} style={{ height: "50px" }}></div>
         </Grid>
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default Catalog
+export default Catalog;
