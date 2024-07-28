0b557bf38cf066759cb3b5b88abfebc45a14ab88
Roman Artemiev <115183967+Roman-Artemiev@users.noreply.github.com>
2024-07-28 19:39:25 +0200

    Save first page for game

--- src/app/catalog/[slug]/page.tsx
+++ src/app/catalog/[slug]/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 import Header from "@/components/header/Header";
-import { Box, Divider, Flex, Heading, Link, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Divider,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import React, { useEffect, useState } from "react";
 import { COLORS, TRANSITIONS } from "@/theme";
@@ -16,13 +24,13 @@ import GameSlider from "@/components/slider/GameSlider";
 import { FaStar } from "react-icons/fa6";
 import FooterBtn from "@/components/footer/FooterBtn";
 
-const  GamePage = () => {
+const GamePage = () => {
   const { slug } = useParams();
   const [data, setData] = useState<Game>();
 
   useEffect(() => {
     (async () => {
-      const [ game, screenshots ] = await Promise.all([
+      const [game, screenshots] = await Promise.all([
         gameDetails({ slug }),
         gameScreenshots({ slug }),
       ]);
@@ -41,14 +49,13 @@ const  GamePage = () => {
       <Header />
 
       <Box className="wrapper" mt="60px">
-        <Flex justifyContent='space-between' mb='40px'>
+        <Flex justifyContent="space-between" mb="40px">
           <Flex
             as={Link}
             href="/catalog"
             textDecoration="none"
             columnGap="20px"
             alignItems="center"
-            
             transition={TRANSITIONS.mainTransition}
             _hover={{
               textDecoration: "none",
@@ -62,62 +69,187 @@ const  GamePage = () => {
             </Text>
           </Flex>
 
-          <Heading as="h2" fontSize="40px" fontWeight="700">{data?.name}</Heading>
+          <Heading as="h2" fontSize="40px" fontWeight="700">
+            {data?.name}
+          </Heading>
         </Flex>
 
-        <Flex justifyContent='space-between' columnGap='30px'>
-          <Box w='70%'>
+        {/* Game slider & about */}
+        <Flex justifyContent="space-between" columnGap="30px">
+          <Box w="70%">
             {data && data.short_screenshots && (
               <GameSlider short_screenshots={data.short_screenshots} />
             )}
 
-            <Flex mt='55px' justifyContent='space-between' alignItems='center' columnGap='24px'>
-              <Flex columnGap='20px'>
+            {/* Game left side, under slider */}
+            <Flex
+              mt='40px'
+              justifyContent="space-between"
+              alignItems="center"
+              columnGap="24px"
+            >
+              <Flex columnGap="20px">
                 <Box>
-                  <Text fontSize='20px' fontWeight='700' mb='5px'>Exceptional</Text>
-                  <Text fontSize='15px' fontWeight='600' letterSpacing='20%' color={COLORS.gray} textTransform='uppercase'>{data?.ratings[0].count} Marks</Text>
+                  <Text fontSize="20px" fontWeight="700" mb="5px">
+                    Exceptional
+                  </Text>
+                  <Text
+                    fontSize="15px"
+                    fontWeight="600"
+                    letterSpacing="20%"
+                    color={COLORS.gray}
+                    textTransform="uppercase"
+                  >
+                    {data?.ratings[0].count} Marks
+                  </Text>
                 </Box>
 
-                <Divider borderRadius='5px' borderColor={COLORS.darkSoft} borderWidth='1px' h='auto' orientation='vertical' />
+                <Divider
+                  borderRadius="5px"
+                  borderColor={COLORS.darkSoft}
+                  borderWidth="1px"
+                  h="auto"
+                  orientation="vertical"
+                />
 
                 <Box>
-                  <Flex alignItems='center' columnGap='6px'>
-                    <Text fontSize='20px' fontWeight='700' mb='5px'>{data?.rating}</Text>
-                    <FaStar size='18px' />
+                  <Flex alignItems="center" columnGap="6px">
+                    <Text fontSize="20px" fontWeight="700" mb="5px">
+                      {data?.rating}
+                    </Text>
+                    <FaStar size="18px" />
                   </Flex>
-                  <Text fontSize='15px' fontWeight='600' letterSpacing='20%' color={COLORS.gray} textTransform='uppercase'>Rating </Text>
+                  <Text
+                    fontSize="15px"
+                    fontWeight="600"
+                    letterSpacing="20%"
+                    color={COLORS.gray}
+                    textTransform="uppercase"
+                  >
+                    Rating{" "}
+                  </Text>
                 </Box>
 
-                <Divider borderRadius='5px' borderColor={COLORS.darkSoft} borderWidth='1px' h='auto' orientation='vertical' />
+                <Divider
+                  borderRadius="5px"
+                  borderColor={COLORS.darkSoft}
+                  borderWidth="1px"
+                  h="auto"
+                  orientation="vertical"
+                />
 
                 <Box>
-                  <Text fontSize='20px' fontWeight='700' mb='5px'>{data?.publishers[0].name}</Text>
-                  <Text fontSize='15px' fontWeight='600' letterSpacing='20%' color={COLORS.gray} textTransform='uppercase'>Publishers</Text>
+                  <Text fontSize="20px" fontWeight="700" mb="5px">
+                    {data?.publishers[0].name}
+                  </Text>
+                  <Text
+                    fontSize="15px"
+                    fontWeight="600"
+                    letterSpacing="20%"
+                    color={COLORS.gray}
+                    textTransform="uppercase"
+                  >
+                    Publishers
+                  </Text>
                 </Box>
               </Flex>
 
-              <FooterBtn pathToIcon={'/icons/medal-star-icon.svg'} alt="Add to favorite"/>
+              <FooterBtn
+                pathToIcon={"/icons/medal-star-icon.svg"}
+                alt="Add to favorite"
+                width="60px"
+                height="60px"
+              />
             </Flex>
-
           </Box>
 
+          <Box w="30%">
+            <Box borderRadius="10px" bgColor={COLORS.dark} px="20px" py="24px" mb='40px'>
+              <Flex
+                mb="15px"
+                justifyContent="space-between"
+                alignItems="center"
+                columnGap="24px"
+              >
+                <Text fontWeight="700" fontSize="24px">
+                  About
+                </Text>
 
-          <Box w='30%' borderRadius='10px' bgColor={COLORS.dark} px='20px' py='24px'>
-            <Flex mb='15px' justifyContent='space-between' alignItems='center' columnGap='24px'>
-              <Text fontWeight='700' fontSize='24px'>About</Text> 
+                <Link href={`${data?.website}`} target="_blank">
+                  <RiExternalLinkLine size="24px" cursor="pointer" />
+                </Link>
+              </Flex>
 
-              <Link href={`${data?.website}`} target="_blank">
-                <RiExternalLinkLine size='24px' cursor='pointer' />
-              </Link>
+              <Box
+                overflow="auto"
+                h="60%"
+                sx={{
+                  "::-webkit-scrollbar": {
+                    width: "6px",
+                    height: "auto",
+                  },
+                  "::-webkit-scrollbar-track": {
+                    background: COLORS.blackLight,
+                    borderRadius: "10px",
+                  },
+                  "::-webkit-scrollbar-thumb": {
+                    background: COLORS.blue,
+                    borderRadius: "10px",
+                  },
+                  '::-webkit-scrollbar-button': {
+                    display: 'none',
+                  },
+                  "::-webkit-scrollbar-thumb:hover": {
+                    background: COLORS.black,
+                  },
+                }}
+              >
+                <Text
+                  h="100%"
+                  fontSize="16px"
+                  color={COLORS.gray}
+                  flexWrap="wrap"
+                  pr="15px"
+                  style={{ textAlign: "justify", hyphens: "auto" }}
+                >
+                  {data?.description_raw
+                    .split("###")
+                    .map((p: string, index: any) => (
+                      <Box as="span" key={index}>
+                        {p}
+                      </Box>
+                    ))}
+                </Text>
+              </Box>
+            </Box>
+
+            {/* Game right side, under about */}
+            <Flex
+              bg={COLORS.darkLight}
+              columnGap="24px"
+              h="60px"
+              px="20px"
+              borderRadius="10px"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Text fontSize="20px" fontWeight="700">
+                ${data?.price}
+              </Text>
+              <Flex columnGap="15px" alignItems="center" cursor="pointer">
+                <Text fontSize="20px" fontWeight="600">
+                  Add to cart
+                </Text>
+                <Image
+                  src={"/icons/bag-icon.svg"}
+                  alt="cart"
+                  w="24px"
+                  h="24px"
+                />
+              </Flex>
             </Flex>
 
-            <Box>
-                <Text fontSize='16px' color={COLORS.gray} flexWrap='wrap'>
-                  {data?.description_raw.split('###').map((p: string, index: any) => (
-                    <Box as='span' key={index}>{p}</Box>
-                  ))}
-                </Text>
-            </Box>
+
           </Box>
         </Flex>
       </Box>
--- src/components/footer/FooterBtn.tsx
+++ src/components/footer/FooterBtn.tsx
@@ -3,15 +3,15 @@ import Image from 'next/image';
 import { Flex, Tooltip } from '@chakra-ui/react';
 import { COLORS, TRANSITIONS } from '@/theme';
 
-const FooterBtn = ({pathToIcon, clue, alt,}: {pathToIcon: string, clue?: string, alt: string, }) => {
+const FooterBtn = ({pathToIcon, clue, alt, width = '44px', height = '44px'}: {pathToIcon: string, clue?: string, alt: string, width?: string, height?: string}) => {
   return (
     <Tooltip label={alt} color={COLORS.white} aria-label={alt} bg={COLORS.darkLight}  placement="bottom">
       <Flex
         alignItems="center"
         cursor="pointer"
         justifyContent="center"
-        w="44px"
-        h="44px"
+        w={width}
+        h={height}
         bgColor={COLORS.darkLight}
         transition={TRANSITIONS.mainTransition}
         _hover={{ transition: TRANSITIONS.mainTransition, bgColor: COLORS.darkSoft, transform: "translateY(-5px)", }}
