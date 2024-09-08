57e7ee266bb5cab1588ac43fab94c482546a652e
Roman Artemiev <115183967+Roman-Artemiev@users.noreply.github.com>
2024-09-08 09:10:38 +0200

    Fix loading for search & try to fix slider in slug page

--- src/app/search/loading.tsx
+++ src/app/search/loading.tsx
@@ -1,11 +1,37 @@
-"use client"
+"use client";
 
-import React from 'react'
+import Header from "@/components/nav/Header";
+import { Box, Flex, Heading, Spinner, Text } from "@chakra-ui/react";
+import { useSearchParams } from "next/navigation";
+import React from "react";
 
 const loading = () => {
-  return (
-    <div>loading</div>
-  )
-}
+  const searchParams = useSearchParams();
+  const search = searchParams.get('search');
 
-export default loading
+  return (
+    <>
+      <Header />
+
+      <Box className="wrapper" h="100%" mt="60px">
+        <Box mb="50px">
+          <Heading as="h1" mb="5px" fontSize="40px" fontWeight="800">
+            Search result
+          </Heading>
+
+          <Text mb="30px" fontSize="16px">
+            {`Based on your search request "${
+              search === "" ? "nothing, so try to look for something" : search
+            }"`}
+          </Text>
+        </Box>
+
+        <Flex justifyContent='center' mt='50px'>
+          <Spinner w='50px' h='50px' />
+        </Flex>
+      </Box>
+    </>
+  );
+};
+
+export default loading;
--- src/components/slider/GameSlider.tsx
+++ src/components/slider/GameSlider.tsx
@@ -6,6 +6,7 @@ import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import "swiper/css";
 import "swiper/css/navigation";
 import { COLORS } from "../../theme";
+import { NavigationOptions } from "swiper/types";
 
 
 const GameSlider = ({ short_screenshots}: { short_screenshots: any }) => {
@@ -15,12 +16,15 @@ const GameSlider = ({ short_screenshots}: { short_screenshots: any }) => {
   return (
     <Swiper
       slidesPerView={1}
-      navigation={{
-        prevEl: prevRef.current,
-        nextEl: nextRef.current,
+      modules={[Navigation]}
+      onBeforeInit={(swiper) => {
+        const navigation = swiper.params.navigation as NavigationOptions; // Приведение navigation к типу NavigationOptions
+        navigation.prevEl = prevRef.current;
+        navigation.nextEl = nextRef.current;
+        swiper.navigation.init();
+        swiper.navigation.update();
       }}
       loop={true}
-      modules={[Navigation]}
       className="mySwiper"
     >
       {short_screenshots.map((screenshot: any) => (
